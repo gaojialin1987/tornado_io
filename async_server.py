47bf3e69e91edf500b156d53fdf00193f5dcb4b4
gaojialin1987 <594616387@qq.com>
2019-03-18 17:11:49 +0800

    添加注释

--- async_server.py
+++ async_server.py
@@ -1,14 +1,18 @@
+# 导入基础包
 import datetime
 import time
 
+# 导入第三方包
 from tornado.web import RequestHandler, Application
 from tornado.options import options, define, parse_command_line
 from tornado.ioloop import IOLoop
 import tornado.gen
 
+# 默认项目配置启动端口
 define('port', default=8000, help='项目默认启动端口')
 
 
+# 定义基础类
 class SleepHandler(RequestHandler):
     @tornado.gen.coroutine
     def get(self):
@@ -17,13 +21,20 @@ class SleepHandler(RequestHandler):
         self.write(str(datetime.datetime.now()))
 
 
+# 主程序入口
 if __name__ == '__main__':
+    # 识别命令行操作
     parse_command_line()
+    # 定义app
     app = Application(
         [
+            # 定义路由
             (r'/sleep', SleepHandler)
         ],
+        # 开启调试
         debug=True,
     )
+    # 启动项目监听端口
     app.listen(options.port)
+    # 启动轮询事件
     IOLoop.current().start()
